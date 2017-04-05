1cc2d307d2298fcbe310a5bd9dd35f7ae017b01c
Freedom <j.doe1337@laposte.net>
2017-04-05 16:09:19 +0200

    Clean beans.User

--- src/main/java/com/alinua/beans/User.java
+++ src/main/java/com/alinua/beans/User.java
@@ -4,152 +4,196 @@ import java.io.Serializable;
 import java.util.Vector;
 
 /**
- * 
+ *
  * @author Yannick ADECHI
  *
  */
 public class User implements Serializable {
 
-	/**
-	 * Default constructor
-	 */
-	public User(){
-		
-	}
-	
-	/**
-	 * Constructor using fields
-	 * 
-	 * @param email
-	 * @param idLinkedin
-	 * @param role
-	 * @param jobAdverts
-	 * @param archivedJobAdverts
-	 */
-	public User(String email, String idLinkedin, Role role, Vector<JobAdvert> jobAdverts,
-			Vector<JobAdvert> archivedJobAdverts) {
-		super();
-		this.email = email;
-		this.idLinkedin = idLinkedin;
-		this.role = role;
-		this.jobAdverts = jobAdverts;
-		this.archivedJobAdverts = archivedJobAdverts;
-	}
-	
-	
-	/**
-	 *  The email of the user
-	 */
-	private String email;
-	
-	/**
-	 * The id of the Linkedin's user
-	 */
-	private String idLinkedin;
-	
-	/**
-	 *  The role of the user
-	 *  @see class Role
-	 */
-	private Role role;
-	
-	/**
-	 *  The list of user's job adverts
-	 *  @see class JobAdvert
-	 */
-	private Vector<JobAdvert> jobAdverts;
-	
-	/**
-	 *  The list of job adverts archived by the user
-	 */
-	private Vector<JobAdvert> archivedJobAdverts;
-	
-	
-	/// GETTERS n SETTERS
-	/**
-	 * @return the email
-	 */
-	public String getEmail() {
-		return email;
-	}
-	
-	/**
-	 * @param email  the email to set
-	 */
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	
-	/**
-	 * @return the idLinkedin
-	 */
-	public String getIdLinkedin() {
-		return idLinkedin;
-	}
-	
-	/**
-	 * @param idLinkedin  the idLinkedin to set
-	 */
-	public void setIdLinkedin(String idLinkedin) {
-		this.idLinkedin = idLinkedin;
-	}
-	
-	/**
-	 * @return the role
-	 * @see class Role
-	 */
-	public Role getRole() {
-		return role;
-	}
-	
-	/**
-	 * @param role  the role to set
-	 * @see class Role
-	 */
-	public void setRole(Role role) {
-		this.role = role;
-	}
-	
-	/**
-	 * @return the jobAdverts
-	 * @see class JobAdvert
-	 */
-	public Vector<JobAdvert> getJobAdverts() {
-		return jobAdverts;
-	}
-	
-	/**
-	 * @param jobAdverts  the jobAdverts to set
-	 * @see class JobAdvert
-	 */
-	public void setJobAdverts(Vector<JobAdvert> jobAdverts) {
-		this.jobAdverts = jobAdverts;
-	}
-	
-	/**
-	 * @return the archivedJobAdverts
-	 * @see class JobAdvert
-	 */
-	public Vector<JobAdvert> getArchivedJobAdverts() {
-		return archivedJobAdverts;
-	}
-	
-	/**
-	 * @param archivedJobAdverts  the archivedJobAdverts to set
-	 * @see class JobAdvert
-	 */
-	public void setArchivedJobAdverts(Vector<JobAdvert> archivedJobAdverts) {
-		this.archivedJobAdverts = archivedJobAdverts;
-	}
+    /**
+     * Default constructor
+     */
+    public User(){
 
-	/* (non-Javadoc)
-	 * @see java.lang.Object#toString()
-	 */
-	@Override
-	public String toString() {
-		return "User [email=" + email + ", idLinkedin=" + idLinkedin + ", role=" + role + ", jobAdverts=" + jobAdverts
-				+ ", archivedJobAdverts=" + archivedJobAdverts + "]";
-	}
-	
-	
+    }
+
+    /**
+     * Constructor using fields
+     *
+     * @param email                 the user email
+     * @param idLinkedin            the Linkedin identifier
+     * @param role                  the user role
+     * @param jobAdverts            the user adverts list
+     * @param archivedJobAdverts    the user archived advert list
+     */
+    public User(String email, String idLinkedin, Role role, Vector<JobAdvert> jobAdverts,
+            Vector<JobAdvert> archivedJobAdverts) {
+        super();
+        this.email = email;
+        this.idLinkedin = idLinkedin;
+        this.role = role;
+        this.jobAdverts = jobAdverts;
+        this.archivedJobAdverts = archivedJobAdverts;
+    }
+
+
+    /**
+     *  The email of the user
+     */
+    private String email;
+
+    /**
+     * The id of the Linkedin's user
+     */
+    private String idLinkedin;
+
+    /**
+     *  The role of the user
+     *
+     *  @see beans.Role
+     */
+    private Role role;
+
+    /**
+     *  The list of user's job adverts
+     *
+     *  @see beans.JobAdvert
+     */
+    private Vector<JobAdvert> jobAdverts;
+
+    /**
+     *  The list of job adverts archived by the user
+     *
+     *  @see beans.JobAdvert
+     */
+    private Vector<JobAdvert> archivedJobAdverts;
+
+
+    /// GETTERS n SETTERS
+    /**
+     * Return the user email
+     *
+     * The string is formated as <identifier>@<domain>
+     *
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * Define a new email for the user
+     *
+     * @param email  the new email
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    /**
+     * Return the user LinkedIn identifier
+     *
+     * @return the Linkedin identifier
+     */
+    public String getIdLinkedin() {
+        return idLinkedin;
+    }
+
+    /**
+     * Define a new LinkedIn identifier for the user
+     *
+     * @param idLinkedin  the new identifier
+     */
+    public void setIdLinkedin(String idLinkedin) {
+        this.idLinkedin = idLinkedin;
+    }
+
+    /**
+     * Return user role status in the platform
+     *
+     * @return the role
+     *
+     * @see beans.Role
+     */
+    public Role getRole() {
+        return role;
+    }
+
+    /**
+     * Define a new role status for the user
+     *
+     * @param role  the new role
+     *
+     * @see beans.Role
+     */
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    /**
+     * Return the user job adverts list
+     *
+     * The adverts are stored in a Vector
+     *
+     * @return the adverts list
+     *
+     * @see beans.JobAdvert
+     */
+    public Vector<JobAdvert> getJobAdverts() {
+        return jobAdverts;
+    }
+
+    /**
+     * Define a new list of job adverts
+     *
+     * The adverts are stored in a Vector
+     *
+     * @param jobAdverts  the new adverts list
+     *
+     * @see beans.JobAdvert
+     */
+    public void setJobAdverts(Vector<JobAdvert> jobAdverts) {
+        this.jobAdverts = jobAdverts;
+    }
+
+    /**
+     * Return the user archived job adverts list
+     *
+     * The adverts are stored in a Vector
+     *
+     * @return the adverts list
+     *
+     * @see beans.JobAdvert
+     */
+    public Vector<JobAdvert> getArchivedJobAdverts() {
+        return archivedJobAdverts;
+    }
+
+    /**
+     * Define a new list of archived job adverts
+     *
+     * The adverts are stored in a Vector
+     *
+     * @param archivedJobAdverts  the new adverts list
+     *
+     * @see beans.JobAdvert
+     */
+    public void setArchivedJobAdverts(Vector<JobAdvert> archivedJobAdverts) {
+        this.archivedJobAdverts = archivedJobAdverts;
+    }
+
+    /**
+     * Return a string which contains object data
+     *
+     * @return the compute string
+     *
+     * @see java.lang.Object#toString()
+     */
+    @Override
+    public String toString() {
+        return "User [email=" + email + ", idLinkedin=" + idLinkedin + ", role=" + role + ", jobAdverts=" + jobAdverts
+                + ", archivedJobAdverts=" + archivedJobAdverts + "]";
+    }
+
+
 }
