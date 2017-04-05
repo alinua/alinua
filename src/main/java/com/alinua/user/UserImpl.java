fac0c60fdea159390bd956c55c365038f3799cab
Pomainfroid <pierre-olivier.mainfroid@etud.univ-angers.fr>
2017-04-05 15:15:42 +0200

    Update UserImpl.java

--- src/main/java/com/alinua/user/UserImpl.java
+++ src/main/java/com/alinua/user/UserImpl.java
@@ -3,10 +3,20 @@ package com.alinua.user;
 import org.apache.log4j.Logger;
 
 
+/**
+         *  Implémentation de l'interface User
+         *         
+         * 
+         */
 public class UserImpl implements User{
 	
 	private static final Logger logger = Logger.getLogger(UserImpl.class);
-
+	
+	/**
+         *  Affiche le User
+         *         
+         * @param none
+         */
 	public void printUser(){
 		System.out.println("printUser() is executed...");
 		logger.info("This is Info message");
