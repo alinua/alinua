a8e78951e20110cd12d32fd2d2f485f7b2310686
Pomainfroid <pierre-olivier.mainfroid@etud.univ-angers.fr>
2017-04-05 15:24:57 +0200

    Update UserAction.java

--- src/main/java/com/alinua/action/UserAction.java
+++ src/main/java/com/alinua/action/UserAction.java
@@ -3,22 +3,37 @@ package com.alinua.action;
 import com.alinua.beans.User;
 import com.alinua.dao.UserDAO;
 
-
+/**
+         *  Classe d'action User
+         *   
+         */
 public class UserAction {
 	
 	private UserDAO userDao;
 	
-		
+	/**
+         *  getter de User DAO
+         *   
+	 * @return {UserDAO} userDao
+         */
 	public UserDAO getUserDAO() {
 		return userDao;
 	}
 
-
+	/**
+         *  setter de User DAO
+         *   
+	 * @param {UserDAO} userDao
+         */
 	public void setUserDAO(UserDAO userDao) {
 		this.userDao = userDao;
 	}
 
-
+	/**
+         *  Ajout de User Action
+         *   
+	 * @return {String} Success or null
+         */
 	public String addUserAction(){
 	
 		///User user=new User("nb", "btynvfg", "btycdeern", "btynnnnnnn", "buyyttyn", "btvvvvvvyn", "btyn111111", "btiiiiiiiyn", "bbbbtyn");
@@ -42,6 +57,12 @@ public class UserAction {
 	
 	}*/
 	
+	/**
+         *  Execution de l'action
+         *   
+	 * @throws Exeption
+	 * @return {String} Success or null
+         */
 	public String execute() throws Exception {
 		
 
