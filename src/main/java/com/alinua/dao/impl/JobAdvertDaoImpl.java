f0ffb4d5cc3a9f07eff2caa4deee79668e78bf71
Pomainfroid <pierre-olivier.mainfroid@etud.univ-angers.fr>
2017-04-05 15:46:31 +0200

    Update JobAdvertDaoImpl.java

--- src/main/java/com/alinua/dao/impl/JobAdvertDaoImpl.java
+++ src/main/java/com/alinua/dao/impl/JobAdvertDaoImpl.java
@@ -7,24 +7,51 @@ import com.alinua.dao.JobAdvertDAO;
 
 public class JobAdvertDaoImpl implements JobAdvertDAO {
 
+	/**
+         *  Save of JobAdvert from current 
+         *  Auto-generated method
+	 *
+	 *  @param {JobAdvert} jobAdvert - Job Advert to save
+	 *  @return {boolean} false
+         */
 	@Override
 	public boolean save(JobAdvert jobAdvert) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+         *  update of JobAdvert from current 
+         *  Auto-generated method
+	 *
+	 *  @param {JobAdvert} jobAdvert - Job Advert to update
+	 *  @return {boolean} false
+         */
 	@Override
 	public boolean update(JobAdvert jobAdvert) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+         *  remove of JobAdvert from current 
+         *  Auto-generated method
+	 *
+	 *  @param {JobAdvert} jobAdvert - Job Advert to remove
+	 *  @return {boolean} false
+         */
 	@Override
 	public boolean remove(JobAdvert jobAdvert) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 
+	/**
+         *  Getter of JobAdverts
+         *  Auto-generated method
+	 *
+	 *  @return {Vector<JobAdvert} null
+         */
 	@Override
 	public Vector<JobAdvert> getJobAdverts() {
 		// TODO Auto-generated method stub
