6b878d34bbec898e8a7fe845f787d2b422f6713d
Lenkonata <mikurokonata@gmail.com>
2017-04-05 14:27:56 +0200

    Doc du projectDAO
    La documentation pour ProjectDAO

--- src/main/java/com/alinua/dao/ProjectDAO.java
+++ src/main/java/com/alinua/dao/ProjectDAO.java
@@ -4,10 +4,48 @@ import java.util.Vector;
 
 import com.alinua.beans.Project;
 
-public interface ProjectDAO {
+/**
+ * 
+ *  Project DAO
+ * 
+ * @author Sullivan 
+ *
+ */
 
+
+public interface ProjectDAO {
+	
+	/**
+	* Take a project as parameter and return as result if the save was a success or not.
+	
+	  @param Project 
+	  
+	  @return true if the save is a success and false if not 
+	**/
 	public boolean save(Project project);
+	
+	/**
+	* Take a project as parameter and return as result if the update of the project was a success or not.
+	
+	  @param Project
+	  
+	  @return true if the update is a success and false if not 
+	**/
 	public boolean update(Project project);
+	
+	/**
+	* Take a project as parameter and return as result if the project was successfully deleted
+	
+	  @param Project
+	  
+	  @return true if the project get removed and false if not 
+	**/
 	public boolean remove(Project project);
+	
+	/**
+	* Return a Vector of all the Projects
+	
+	  @return a list of Project
+	**/
 	public Vector<Project> getProjects();
 }
