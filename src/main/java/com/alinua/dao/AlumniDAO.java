2bb295b3714d3d075a8be761c39e01b018afab43
Lenkonata <mikurokonata@gmail.com>
2017-04-05 15:15:34 +0200

    Documentation des Alumni Dao
    La documentation pour la DAO des alumni

--- src/main/java/com/alinua/dao/AlumniDAO.java
+++ src/main/java/com/alinua/dao/AlumniDAO.java
@@ -3,10 +3,47 @@ package com.alinua.dao;
 import java.util.Vector;
 import com.alinua.beans.Alumni;
 
+
+/**
+ * 
+ *  DAO of the Alumni
+ * 
+ * @author Sullivan 
+ *
+ */
 public interface AlumniDAO {
 
+	/**
+	* Take an alumni as parameter and return as result if the save was a success or not.
+	*
+	*  @param Alumni 
+	*  
+	*  @return true if the save is a success and false if not 
+	**/
 	public boolean save(Alumni alumni);
+	
+	/**
+	* Take an alumni as parameter and return as result if the update was a success or not.
+	*
+	*  @param Alumni 
+	*  
+	*  @return true if the update is a success and false if not 
+	**/
 	public boolean update(Alumni alumni);
+	
+	/**
+	* Take an alumni as parameter and return as result if the supression was a success or not.
+	*
+	*  @param Alumni 
+	*  
+	*  @return true if the supression is a success and false if not 
+	**/
 	public boolean remove(Alumni alumni);
+	
+	/**
+	* Return a Vector of all the Alumnis
+	
+	  @return a list of Alumnis
+	**/
 	public Vector<Alumni> getAlumnis();
 }
