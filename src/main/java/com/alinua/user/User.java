fbe9838418ce05ccd6f7570e144c7c38b1856d43
Pomainfroid <pierre-olivier.mainfroid@etud.univ-angers.fr>
2017-04-05 15:13:21 +0200

    Update User.java

--- src/main/java/com/alinua/user/User.java
+++ src/main/java/com/alinua/user/User.java
@@ -1,7 +1,18 @@
 package com.alinua.user;
 
+/**
+         *  Interface User (public)
+         *         
+         * 
+         */
 public interface User {
 
+/**
+         *  Void Afficher User
+         *         
+         * @param none
+         */
 	public void printUser();
 
 }
+
