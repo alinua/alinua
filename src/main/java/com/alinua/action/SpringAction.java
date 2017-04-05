40d0705adf8f2d39a89a3faa6457c928ca0178f7
Pomainfroid <pierre-olivier.mainfroid@etud.univ-angers.fr>
2017-04-05 15:21:28 +0200

    Update SpringAction.java

--- src/main/java/com/alinua/action/SpringAction.java
+++ src/main/java/com/alinua/action/SpringAction.java
@@ -9,22 +9,42 @@ import java.io.FileReader;
 import org.json.simple.JSONArray;
 import org.json.simple.parser.JSONParser;
 
+
+/**
+         *  Classe d'action Spring 
+         *   
+	 * @author pomainfroid
+         * @version 1.0
+         */
 public class SpringAction {
 
 	//DI via Spring	
 	private JSONArray annonceJson;
 	
 	
-
+	/**
+         *  setter d'annonce Json
+         *   
+	 * @param {JSONArray} annonceJson 
+         */
 	public void setAnnonceJson(JSONArray annonceJson) {
 		this.annonceJson = annonceJson;
 	}
-	
+	/**
+         *  getter d'annonce Json
+         *   
+	 * @return {JSONArray} annonceJson 
+         */
 	public JSONArray getAnnonceJson() {
 		return annonceJson;
 	}
 
-	
+	/**
+         *  getter d'annonce Json
+         *   
+	 * @throw Exception 
+	 * @return {String} Success or null 
+         */
 	public String getJson(){
 		annonceJson=new JSONArray();
 		JSONParser parser= new JSONParser();
@@ -41,7 +61,12 @@ public class SpringAction {
 	}
 
 
-
+	/**
+         * Fonction d'execution 
+         *   
+	 * @throw Exception 
+	 * @return {String} Success or null 
+         */
 	public String execute() throws Exception {		
 
 		System.out.println(" execute() ");
